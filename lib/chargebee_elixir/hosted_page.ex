a4bb1d6eba7b4f6ab2a476c4e17b69f76d28a042
fantypants <8563615@gmail.com>
2022-06-06 14:16:20 +0100

    Before new Release

--- lib/chargebee_elixir/hosted_page.ex
+++ lib/chargebee_elixir/hosted_page.ex
@@ -3,4 +3,6 @@ defmodule ChargebeeElixir.HostedPage do
 
   def checkout_new(params) do create(params, "/checkout_new") end
   def checkout_existing(params) do create(params, "/checkout_existing") end
+  def update_payment_method(params) do create(params, "/update_payment_method") end
+  def manage_payment_sources(params) do create(params, "/manage_payment_sources") end
 end
